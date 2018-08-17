9c8a1609322835062ce71581208caf14bcec4464
tangrela <tumbzzc@qq.com>
2018-08-17 11:54:57 +0800

    添加91porn脚本

---
+++ 91porn/downloader.py
@@ -0,0 +1,67 @@
+#-*- coding=utf-8 -*-
+import requests
+import threading
+import os
+import random
+import math
+
+def randip():
+    return str(random.randint(0, 255)) + "."\
+            + str(random.randint(0, 255)) + "."\
+            + str(random.randint(0, 255)) + "."\
+            + str(random.randint(0, 255))
+
+
+class downloader:
+    def __init__(self,url,path,picture,title,id,offset=327680*10):
+        self.url=url
+        self.offset=offset
+        self.path=path
+        self.picture=picture
+        self.title=title
+        self.id=id
+        r = requests.head(self.url)
+        self.total = int(r.headers['Content-Length'])
+        print(u'{} total size is {}'.format(self.title,self.total))
+
+    def get_range(self):
+        ranges=[]
+        self.num=math.floor(self.total/self.offset)+1
+        for i in range(self.num):
+            if i==self.num-1:
+                ranges.append((i*self.offset,self.total))
+            else:
+                ranges.append((i*self.offset,(i+1)*self.offset))
+        return ranges
+
+    def download(self,start,end):
+        #拼接Range字段,accept字段支持所有编码
+        headers={'Range':'Bytes=%s-%s' % (start,end),'Accept-Encoding':'*','X-Forwarded-For':randip()}
+        res = requests.get(self.url,headers=headers)
+        print(u'{} download percent {}%'.format(self.title,round(float(end)/self.total,3)*100))
+        #seek到start位置
+        self.fd.seek(start)
+        self.fd.write(res.content)
+
+    def run(self):
+        # 保存文件打开对象
+        self.fd =  open(self.path,'w')
+        thread_list = []
+        #一个数字,用来标记打印每个线程
+        n = 0
+        for ran in self.get_range():
+            start,end = ran
+            n+=1
+            #创建线程 传参,处理函数为download
+            thread = threading.Thread(target=self.download,args=(start,end))
+            #启动
+            thread.start()
+            thread_list.append(thread)
+        for i in thread_list:
+            # 设置等待
+            i.join()
+        print(u'download %s load success'%(self.path))
+        #关闭文件
+        self.fd.close()
+        with open('history.txt','a') as f:
+            f.write('{}\n'.format(self.id))
---
+++ 91porn/porn91.py
@@ -0,0 +1,123 @@
+#-*- coding=utf-8 -*-
+import requests
+import re
+import os
+import sys
+import argparse
+import datetime
+import logging
+import time
+import random
+import string
+import threading
+from downloader import downloader
+
+headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/45.0.2454.101 Safari/537.36',
+           'X-Forwarded-For': '', 'Accept-Language': 'zh-CN,zh;q=0.8'}
+info_reg = re.compile(
+    '''<div class="imagechannel[\W\w]*?<a target=blank href="(http://.*?/view_video.*?\.php\?viewkey=.*?)">[\W\w]*?<img src="(.*?)" width="120" title="(.*?)" />''')
+mp4_reg = re.compile("""<source src="(.*?)" type='video/mp4'>""")
+chars = string.ascii_letters
+
+basedir=os.path.abspath('.')
+video_path=os.path.join(basedir,'video')
+if not os.path.exists(video_path):
+    os.mkdir(video_path)
+
+#找国内可用网址到这里：https://www.ebay.com/usr/91dizhi_1
+porn91_url = 'http://93.91p09.space/'
+info_list=[]
+
+
+def timenow():
+    return datetime.datetime.now().strftime('%Y%m%d %H:%M:%S')
+
+
+def randip():
+    return str(random.randint(0, 255)) + "."\
+            + str(random.randint(0, 255)) + "."\
+            + str(random.randint(0, 255)) + "."\
+            + str(random.randint(0, 255))
+
+def randchar(): return ''.join(random.sample(chars, 5))
+
+def exists(id):
+    if not os.path.exists('history.txt'):
+        return False
+    with open('history.txt','r') as f:
+        history=[i.strip() for i in f.readlines()]
+    if id in history:
+        return True
+    else:
+        return False
+
+def get_list(url):
+    print('start parse ' + url)
+    videos=[]
+    try:
+        headers['X-Forwarded-For'] = randip()
+        resp = requests.get(url, headers=headers)
+        resp.encoding='utf-8'
+        cont = resp.text
+        urls = info_reg.findall(cont)
+        print(url+ ' get {} videos'.format(len(urls)))
+        for ul in urls:
+            url, picture, title = ul
+            url = url.replace('_hd', '')
+            id = re.findall('viewkey=(.*?)&', url)[0]
+            downpath=os.path.join(video_path,'{}.mp4'.format(id))
+            if not exists(id):
+                videos.append({'id':id, 'url':url, 'picture':picture, 'downpath':downpath,'title':title})
+                print(id + ' do not exists!')
+        return videos
+    except Exception as e:
+        print(e)
+        return False
+
+
+def download_video(**kwargs):
+    ds=[]
+    url=kwargs['url']
+    print('geting video from url {}'.format(url))
+    try:
+        headers['X-Forwarded-For'] = randip()
+        resp = requests.get(url, headers=headers)
+        resp.encoding='utf-8'
+        cont = resp.text
+        video = mp4_reg.findall(cont)[0]
+        d=downloader(url=video,path=kwargs['downpath'],picture=kwargs['picture'],title=kwargs['title'],id=kwargs['id'])
+        return d
+    except Exception as e:
+        print(e)
+        return False
+
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-t', default='new')
+    parser.add_argument('-a1', default=1, type=int)
+    parser.add_argument('-a2', default=1, type=int)
+    args = parser.parse_args()
+    urls = {porn91_url: [args.a1, args.a2]}
+
+    pagelists=[]
+    for url in urls.keys():
+        posts_url = url + '/video.php?viewtype=basic&category=hd&page=%d'
+        for i in range(urls[url][0], urls[url][1] + 1):
+            page_url = posts_url % i
+            pagelists.append(page_url)
+
+    #一页一页下载
+    for page in pagelists:
+        videopages=get_list(page)
+        if videopages!=False:
+            tasks=[]
+            for video in videopages:
+                d=download_video(**video)
+                d.run()
+        else:
+            print('get list fail!')
+
+
+if __name__=='__main__':
+    main()
